8724c0a792ca8869a4decba0ef399f35cb70e2dc
Steva <steva57@rambler.ru>
2019-04-15 16:25:48 +0500

    inheritance

---
+++ Snake/Snake/Figura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Figura
+    {
+        protected List<Point> pList;
+
+        public void Drow()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
---
+++ Snake/Snake/HorizontalLlne.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class HorizontalLlne: Figura
+    {
+  
+        public HorizontalLlne(int xLeft, int xReight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for (int x = xLeft; x <= xReight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+
+            }
+        
+        }
+      }
+}
